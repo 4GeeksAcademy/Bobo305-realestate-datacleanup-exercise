e1435588c209a1ffbdc8114054ad03a44637b2ca
bobo305 <126503694+bobo305@users.noreply.github.com>
2023-09-02 15:33:57 +0000

    need to double check almost done

---
+++ somee.py
@@ -0,0 +1,12 @@
+import pandas as pd
+ds = pd.read_csv('assets/real_estate.csv', sep=';')
+
+population_counts = ds['level5'].value_counts()
+
+
+most_houses_population = population_counts.idxmax()
+num_houses_most_population = population_counts.max()
+
+#the results
+print("Population with the Most Houses:", most_houses_population)
+print("Number of Houses in the Most Populated Population:", num_houses_most_population)
